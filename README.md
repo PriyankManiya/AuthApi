1485569432b423870352954c5a2430d942517a76
Priyank <priyankmaniya199@gmail.com>
2022-12-23 14:55:06 -0700

    migration to POSTGRESQL

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 files to Import in Postman
  1. DjangoAuthApi.postman_collection.json
  2. DjangoAuthApi.postman_environment.json
+
+# Migrated SqLite to PostGreSQL Steps
+python3 manage.py dumpdata > data.json
+python3 manage.py migrate --run-syncdb
+python3 manage.py shell
+from django.contrib.contenttypes.models import ContentType
+ContentType.objects.all().delete()
+exit()
+python3 manage.py loaddata data.json
--- djangoAuthApi/djangoAuthApi/settings.py
+++ djangoAuthApi/djangoAuthApi/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'djangoAuthApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'priyankmaniya',
+        'USER': 'priyankmaniya',
+        # 'PASSWORD': 'priyankmaniya',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
